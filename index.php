917aaf19b191368602c124cdf817d053d0a5ddd5
johnluk0092 <60909940+johnluk0092@users.noreply.github.com>
2025-03-25 23:02:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require('include/header.php');
+require('include/sidebar.php');
+?>
+<body>
+	<a>THIS IS HOME PAGE</a>
+</body>
+<!-- Main Content End -->
+
+<?php
+require('include/footer.php');
+require('include/script.php');
+?>
---
+++ index2.php
@@ -0,0 +1,18 @@
+<?php
+// Include the header
+require('include/header.php');
+require('include/sidebar.php');
+?>
+
+
+<body>
+	<h1>WELLCOME</h1>
+
+</body>
+<?php
+// Include the footer
+require('include/footer.php');
+
+// Include the scripts
+require('include/script.php');
+?>
