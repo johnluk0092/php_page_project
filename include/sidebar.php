87b734012f5b72d5e37be2ab1c7638da6f413988
johnluk0092 <60909940+johnluk0092@users.noreply.github.com>
2025-03-25 23:00:22 +0700

    Add files via upload

---
+++ include/sidebar.php
@@ -0,0 +1,87 @@
+    <!-- Hero Section Begin -->
+<!--    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="hero__categories">
+                        <div class="hero__categories__all">
+                            <i class="fa fa-bars"></i>
+                            <span>Danh mục sản phẩm</span>
+                        </div>
+                        <ul>
+                            <li><a href="#">Quần áo thể thao</a></li>
+                            <li><a href="#">Giày Chạy Bộ</a></li>
+                            <li><a href="#">Giày Bóng Đá</a></li>
+                            <li><a href="#">Giày Cầu Lông</a></li>
+                            <li><a href="#">Giày Quần Vợt/Tenis</a></li>
+                            <li><a href="#">Giày Pickleball</a></li>
+                            <li><a href="#">Vợt Quần Vợt/Tennis</a></li>
+                            <li><a href="#">Vợt Cầu Lông</a></li>
+                            <li><a href="#">Vợt Pickleball</a></li>
+                            <li><a href="#">Bao lô</a></li>
+                            <li><a href="#">Phụ kiện</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>-->
+    <!-- Hero Section End -->
+<section class="hero">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="hero__categories">
+                    <div class="hero__categories__all">
+                        <i class="fa fa-bars"></i>
+                        <span>Danh mục sản phẩm</span>
+                    </div>
+                    <ul>
+                        <li><a href="#">Quần áo thể thao</a></li>
+                        <li><a href="#">Giày Chạy Bộ</a></li>
+                        <li><a href="#">Giày Bóng Đá</a></li>
+                        <li><a href="#">Giày Cầu Lông</a></li>
+                        <li><a href="#">Giày Quần Vợt/Tenis</a></li>
+                        <li><a href="#">Giày Pickleball</a></li>
+                        <li><a href="#">Vợt Quần Vợt/Tennis</a></li>
+                        <li><a href="#">Vợt Cầu Lông</a></li>
+                        <li><a href="#">Vợt Pickleball</a></li>
+                        <li><a href="#">Bao lô</a></li>
+                        <li><a href="#">Phụ kiện</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="hero__search">
+                    <div class="hero__search__form">
+                        <form action="#">
+                            <div class="hero__search__categories">
+                                All Categories
+                                <span class="arrow_carrot-down"></span>
+                            </div>
+                            <input type="text" placeholder="What do you need?">
+                            <button type="submit" class="site-btn">SEARCH</button>
+                        </form>
+                    </div>
+                    <div class="hero__search__phone">
+                        <div class="hero__search__phone__icon">
+                            <i class="fa fa-phone"></i>
+                        </div>
+                        <div class="hero__search__phone__text">
+                            <h5>+65 11.188.888</h5>
+                            <span>support 24/7 time</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="hero__item set-bg" data-setbg="img/hero/banner1.jpg">
+                    <div class="hero__text">
+                        <span>Nhóm 7</span>
+                        <h2>Website bán đồ thể thao <br />Đồ án cuối kỳ</h2>
+                        <p>Thực hành phát triển mã nguồn mở</p>
+                        <a href="#" class="primary-btn">SHOP NOW</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
